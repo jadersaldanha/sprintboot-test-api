e02e622bdb411a9164c49f8522e47f71e5bb96ff
Jader de Freitas Saldanha <jader_saldanha@terceiros.sicredi.com.br>
2020-06-02 15:47:33 -0300

    inciando projeto em spring

---
+++ src/main/java/com/sprintboottestapi/sprintboottestapi/SprintbootTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sprintboottestapi.sprintboottestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootTestApiApplication.class, args);
+	}
+
+}
